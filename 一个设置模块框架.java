179c6d5c8702db2392106705dfcdc2a3ef7a7bd4
ChanNagihong <403889292@qq.com>
2015-10-15 15:01:06 +0800

    Update and rename 一个设置模块框架.markdown to 一个设置模块框架.java

--- 一个设置模块框架.java
+++ 一个设置模块框架.java
@@ -1,19 +1,12 @@
-> 首先，每个Option对应每一项设置
-       
->       读取设置值 Options.read()
-
->       更改设置值 Options.write()
-
-> 然后将所有设置放到一个Map<String, Options>中管理
+首先，每个Option对应每一项设置
+       读取设置值 Options.read()
+       更改设置值 Options.write()
+然后将所有设置放到一个Map<String, Options>中管理
  
-> 优点：
+优点：
+       1、灵活，方便整体操作，如backup、restore
+       2、扩展性高，可以扩展成类似权限模型配置的工具，如管理员权限模型、普通用户权限模型，识别身份后可直接进行对应权限设置
 
->      灵活，方便整体操作，如backup、restore
-
->      扩展性高，可以扩展成类似权限模型配置的工具，如管理员权限模型、普通用户权限模型，识别身份后可直接进行对应权限设置
-
-
-```
 public class Option {
   private final String fieldName;
   private final String setterName;
@@ -77,21 +70,17 @@ public class Option {
     }
   }
 }
-```
 
 > config_defaults.xml
 
-```
 <resources>
   <bool name="field_boolean">true</bool>
   <integer name="field_int">1</integer>
   <string name="field_string">goodbyeworld</string>
 </resources>
-```
 
 > Config.java
 
-```
 public class Config {
     
   private boolean field_boolean;
@@ -121,11 +110,9 @@ public class Config {
                                       .setDefault(R.int.field_int);
     }
   }
-```
 
 > init
 
-```
   void init() {
     Resources res = context.getResources();
       for(Map.Entry<String, Option> entry : getMap().entrySet()) {
@@ -133,11 +120,9 @@ public class Config {
         Option options = entry.getValue();
     }
   }
-```
 
 > backup
 
-```
   void backup() {
     JSONObject json = new JSONObject();
     for(Map.Entry<String, Option> entry : getMap().entrySet()) {
@@ -145,11 +130,9 @@ public class Config {
     }
     //save json to file or, etc.
   }
-```
 
 > restore
 
-```
   void restore() {
     JSONObject json = new JSONObject(jsonStr);
     Iterator<String> iter = json.keys();
@@ -160,4 +143,3 @@ public class Config {
       option.write(classObject); // here classObject is Config
     }
   }
-```
